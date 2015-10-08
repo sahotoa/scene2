aac76c78f3cefa1febbed43fdd515f2334b92247
sahotoa <sahotoa@gmail.com>
2015-10-08 15:38:16 +0700

    Actions 00

--- core/src/com/mygdx/scene2/SceneTwo.java
+++ core/src/com/mygdx/scene2/SceneTwo.java
@@ -1,17 +1,22 @@
 package com.mygdx.scene2;
 
+import java.util.Iterator;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
+import com.badlogic.gdx.scenes.scene2d.actions.RotateToAction;
+import com.badlogic.gdx.scenes.scene2d.actions.ScaleToAction;
 import com.mygdx.scene2.SceneTwo.MyActor;
 
 public class SceneTwo extends ApplicationAdapter {
@@ -34,15 +39,17 @@ public class SceneTwo extends ApplicationAdapter {
 		
 		@Override
 		public void draw(Batch batch, float alpha){
-			batch.draw(texture,this.getX(),this.getY());
+			batch.draw(texture,this.getX(),getY(),this.getOriginX(),this.getOriginY(),this.getWidth(),
+		            this.getHeight(),this.getScaleX(), this.getScaleY(),this.getRotation(),0,0,
+		            texture.getWidth(),texture.getHeight(),false,false);
 		}
 		
-		@Override
+		/*@Override
 		public void act(float delta){
-			if (started){
-				actorX+=5;
-			}
-		}
+		    for(Iterator<Action> iter = this.getActions().iterator(); iter.hasNext();){
+		        iter.next().act(delta);
+		    }
+		}*/
 	}
 	
 	private Stage stage;
@@ -55,9 +62,19 @@ public class SceneTwo extends ApplicationAdapter {
 		MyActor myActor = new MyActor();
 		/*myActor.setTouchable(Touchable.enabled);*/
 		MoveToAction moveAction = new MoveToAction();
+		RotateToAction rotateAction = new RotateToAction();
+		ScaleToAction scaleAction = new ScaleToAction();
+		
 		moveAction.setPosition(300f, 0f);
-		moveAction.setDuration(10f);
+		moveAction.setDuration(5f);
+		rotateAction.setRotation(90f);
+		rotateAction.setDuration(5f);
+		scaleAction.setScale(0.5f);
+		scaleAction.setDuration(5f);
+		
 		myActor.addAction(moveAction);
+		myActor.addAction(rotateAction);
+		myActor.addAction(scaleAction);
 		
 		stage.addActor(myActor);
 	}
